7067224684dc62d7b1f58167183e2b7770a45e48
Paul Miller <paul@paulmillr.com>
2016-02-29 02:04:10 +0200

    Fix URLs.

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -9,10 +9,9 @@ export default class App extends React.Component {
         <Counter />
         <h2>Welcome!</h2>
         <ul>
-          <li><a href="http://brunch.readthedocs.org/">Documentation</a></li>
-          <li><a href="https://github.com/brunch/brunch/issues">Github Issues</a></li>
-          <li><a href="https://github.com/brunch/twitter">Twitter Example App</a></li>
-          <li><a href="https://github.com/brunch/todos">Todos Example App</a></li>
+          <li><a href="http://brunch.io/">Brunch</a></li>
+          <li><a href="https://facebook.github.io/react/">React</a></li>
+          <li><a href="http://redux.js.org">Redux</a></li>
         </ul>
       </div>
     );
