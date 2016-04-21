949cdacced48b34eed4bdbbe9128310d0314f431
Gosha Arinich <me@goshakkk.name>
2016-04-21 16:59:05 +0300

    Updates for HMR

--- app/initialize.js
+++ app/initialize.js
@@ -5,19 +5,31 @@ import { createStore } from 'redux';
 import counterApp from './reducers';
 import App from 'components/App';
 
-const store = createStore(counterApp, 0);
+const store = createStore(counterApp, module.hot && module.hot.data && module.hot.data.counter || 0);
 
 if (module.hot) {
   module.hot.accept('./reducers', () => {
     store.replaceReducer(require('./reducers').default);
   });
+  module.hot.accept();
+
+  module.hot.dispose((data) => {
+    data.counter = store.getState();
+    [].slice.apply(document.querySelector('#app').children).forEach(function(c) { c.remove() });
+  });
 }
 
-document.addEventListener('DOMContentLoaded', () => {
+const load = () => {
   ReactDOM.render(
     <Provider store={store}>
       <App />
     </Provider>,
     document.querySelector('#app')
   );
-});
+};
+
+if (document.readyState !== 'complete') {
+  document.addEventListener('DOMContentLoaded', load);
+} else {
+  load();
+}
